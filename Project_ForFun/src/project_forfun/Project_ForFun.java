52869ebfad5c833ed48031beb43cc794cae5f162
Wytze Jelle Bruinsma <wytzejelle@hotmail.com>
2015-04-30 12:49:22 +0200

    test
    mooi

--- Project_ForFun/src/project_forfun/Project_ForFun.java
+++ Project_ForFun/src/project_forfun/Project_ForFun.java
@@ -14,6 +14,6 @@ public class Project_ForFun {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello world");
     }
 }
