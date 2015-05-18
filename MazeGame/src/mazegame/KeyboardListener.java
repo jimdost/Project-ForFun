44cf385945174b2d156d963e4f7c73fc5656a56d
jimdost <jim_master@hotmail.com>
2015-05-18 13:47:07 +0200

    Revert "Keyboard listener werkt"
    This reverts commit d25a55f2fb92d7b1a943c75abd5a6eacbfaf1d63.

--- MazeGame/src/mazegame/KeyboardListener.java
+++ MazeGame/src/mazegame/KeyboardListener.java
@@ -4,64 +4,18 @@
  * and open the template in the editor.
  */
 package mazegame;
-
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  *
  * @author Jim
  */
-public class KeyboardListener extends JFrame implements KeyListener {
+public class KeyboardListener implements ActionListener {
 
-    private static JLabel label;
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
-     public KeyboardListener(String s) {
-        super(s);
-        JPanel p = new JPanel();
-        label = new JLabel("Key Listener!");
-        p.add(label);
-        add(p);
-        addKeyListener(this);
-        setSize(200, 100);
-        setVisible(true);
-
-    }
-            
-    @Override
-    public void keyTyped(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            System.out.println("Right key typed");
-        }
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            System.out.println("Left key typed");
-        }
-    }
- 
-    @Override
-    public void keyPressed(KeyEvent e) {
-       if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            System.out.println("Right key pressed");
-        }
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            System.out.println("Left key pressed");
-        }
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            System.out.println("Right key Released");
-        }
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            System.out.println("Left key Released");
-        }
-    }
-    public static void main(String[] args) {
-        new KeyboardListener("Key Listener Tester");
-    }
 }
