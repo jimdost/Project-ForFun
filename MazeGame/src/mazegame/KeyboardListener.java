ee514d33ad0a9d019dabace012bf0410498f69ca
jimdost <jim_master@hotmail.com>
2015-05-18 13:16:46 +0200

    Ok

---
+++ MazeGame/src/mazegame/KeyboardListener.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mazegame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Jim
+ */
+public class KeyboardListener implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
