7a83a848650d5fdd7cf2355f10bd9ab4b16dc733
jimdost <jim_master@hotmail.com>
2015-05-19 14:08:45 +0200

    Forfun Objecten

---
+++ Project_ForFun/src/project_forfun/Element.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_forfun;
+
+import java.awt.*;
+
+/**
+ *
+ * @author Jim
+ */
+public abstract class Element
+{
+    private int posX;
+    private int posY;
+    private Graphics afbeelding;
+    
+    public void getPosX()
+    {
+        
+    }
+    public void setPosX()
+    {
+        
+    }
+    public void getPosY()
+    {
+        
+    }
+    public void setPosY()
+    {
+        
+    }
+}
---
+++ Project_ForFun/src/project_forfun/GameCharacter.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_forfun;
+
+/**
+ *
+ * @author Jim
+ */
+public class GameCharacter extends Element
+{
+    public void bewegen()
+    {
+        
+    }
+    public void oppakken()
+    {
+        
+    }
+    public void vuurRaket()
+    {
+        
+    }
+}
