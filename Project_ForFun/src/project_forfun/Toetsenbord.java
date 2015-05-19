62f4fe3b084f3742eea44555b868c0f75f29f91c
jimdost <jim_master@hotmail.com>
2015-05-19 14:13:55 +0200

    Toetsenbord

--- Project_ForFun/src/project_forfun/Toetsenbord.java
+++ Project_ForFun/src/project_forfun/Toetsenbord.java
@@ -5,11 +5,62 @@
  */
 package project_forfun;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Jim
  */
-public class Toetsenbord
+public class Toetsenbord extends JFrame implements KeyListener
 {
+    private static JLabel label;
     
+    public Toetsenbord()
+    {
+        super();
+        JPanel p = new JPanel();
+        label = new JLabel("Key Listener!");
+        p.add(label);
+        add(p);
+        addKeyListener(this);
+        setSize(200, 100);
+        setVisible(true);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e)
+    {
+       
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e)
+    {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            System.out.println("Right");
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            System.out.println("Lef");
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            System.out.println("Up");
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            System.out.println("Down");
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e)
+    {
+        
+    }
+    public static void main(String[] args) 
+    {
+        new Toetsenbord();
+    }
 }
